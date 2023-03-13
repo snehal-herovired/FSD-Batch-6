d44e3a644726b732c5aeef6b26d8c3dae2820f6e
Mobasshir Hasan <92921221+Mobasshir98@users.noreply.github.com>
2023-03-13 12:37:18 +0530

    question 1 updated

---
+++ Full fledged Web-page/Question1/script.js
@@ -0,0 +1,11 @@
+
+
+
+const burger = document.querySelector(".burger");
+const navLinks = document.querySelector(".nav-links");
+
+burger.addEventListener("click", () => {
+  navLinks.classList.toggle("nav-active");
+  burger.classList.toggle("toggle");
+});
+
